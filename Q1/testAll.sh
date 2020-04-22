8954e8dcc1d7c26e1cb13ec037f5d48126b9b488
Jacob Zahn <jmzahn@satx.rr.com>
2020-04-22 16:53:16 -0500

    add test All scripts

---
+++ Q1/testAll.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e "\nTest i\n"
+./i/test.sh
+
+echo -e "\nTest ii\n"
+./ii/test.sh
+
+echo -e "\nTest iii\n"
+./iii/test.sh
---
+++ Q2/testAll.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo -e "\nTest gauss\n"
+./test.sh
+echo -e "\nTest LU\n"
+./testLU.sh
