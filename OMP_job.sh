a71af3d6781773442c28af0aa91fbe775b4db63e
Jacob Zahn <jmzahn@satx.rr.com>
2020-04-22 17:23:57 -0500

    update job script

--- OMP_job.sh
+++ OMP_job.sh
@@ -14,7 +14,81 @@
 module load gnu7 
 
 echo "\nTest Q1\n"
-./Q1/testAll.sh
+
+echo -e "\nTest i\n"
+
+echo -e "\nTest with 1 proc\n"
+./Q1/i/MMProduct 2000 1 > ./Q1/i/testout/MMProduct_2000_01_quanah
+echo -e "\nTest with 2 proc\n"
+./Q1/i/MMProduct 2000 2 > ./Q1/i/testout/MMProduct_2000_02_quanah
+echo -e "\nTest with 4 proc\n"
+./Q1/i/MMProduct 2000 4 > ./Q1/i/testout/MMProduct_2000_04_quanah
+echo -e "\nTest with 8 proc\n"
+./Q1/i/MMProduct 2000 8 > ./Q1/i/testout/MMProduct_2000_08_quanah
+echo -e "\nTest with 16 proc\n"
+./Q1/i/MMProduct 2000 16 > ./Q1/i/testout/MMProduct_2000_16_quanah
+echo -e "\nTest with 32 proc\n"
+./Q1/i/MMProduct 2000 32 > ./Q1/i/testout/MMProduct_2000_32_quanah
+
+echo -e "\nTest ii\n"
+
+echo -e "\nTest with 1 proc\n"
+./Q1/ii/MMProduct 2000 1 > ./Q1/ii/testout/MMProduct_2000_01_quanah
+echo -e "\nTest with 2 proc\n"
+./Q1/ii/MMProduct 2000 2 > ./Q1/ii/testout/MMProduct_2000_02_quanah
+echo -e "\nTest with 4 proc\n"
+./Q1/ii/MMProduct 2000 4 > ./Q1/ii/testout/MMProduct_2000_04_quanah
+echo -e "\nTest with 8 proc\n"
+./Q1/ii/MMProduct 2000 8 > ./Q1/ii/testout/MMProduct_2000_08_quanah
+echo -e "\nTest with 16 proc\n"
+./Q1/ii/MMProduct 2000 16 > ./Q1/ii/testout/MMProduct_2000_16_quanah
+echo -e "\nTest with 32 proc\n"
+./Q1/ii/MMProduct 2000 32 > ./Q1/ii/testout/MMProduct_2000_32_quanah
+
+echo -e "\nTest iii\n"
+
+echo -e "\nTest with 1 proc\n"
+./Q1/iii/MMProduct 2000 1 > ./Q1/iii/testout/MMProduct_2000_01_quanah
+echo -e "\nTest with 2 proc\n"
+./Q1/iii/MMProduct 2000 2 > ./Q1/iii/testout/MMProduct_2000_02_quanah
+echo -e "\nTest with 4 proc\n"
+./Q1/iii/MMProduct 2000 4 > ./Q1/iii/testout/MMProduct_2000_04_quanah
+echo -e "\nTest with 8 proc\n"
+./Q1/iii/MMProduct 2000 8 > ./Q1/iii/testout/MMProduct_2000_08_quanah
+echo -e "\nTest with 16 proc\n"
+./Q1/iii/MMProduct 2000 16 > ./Q1/iii/testout/MMProduct_2000_16_quanah
+echo -e "\nTest with 32 proc\n"
+./Q1/iii/MMProduct 2000 32 > ./Q1/iii/testout/MMProduct_2000_32_quanah
+
 
 echo "\nTest Q2\n"
-./Q2/testAll.sh
+
+echo -e "\nTest gauss\n"
+
+echo -e "\nTest with 1 proc\n"
+./Q2/gaussOmp 2000 1 > ./Q2/testout/gaussOmp_2000_01_quanah
+echo -e "\nTest with 2 proc\n"
+./Q2/gaussOmp 2000 2 > ./Q2/testout/gaussOmp_2000_02_quanah
+echo -e "\nTest with 4 proc\n"
+./Q2/gaussOmp 2000 4 > ./Q2/testout/gaussOmp_2000_04_quanah
+echo -e "\nTest with 8 proc\n"
+./Q2/gaussOmp 2000 8 > ./Q2/testout/gaussOmp_2000_08_quanah
+echo -e "\nTest with 16 proc\n"
+./Q2/gaussOmp 2000 16 > ./Q2/testout/gaussOmp_2000_16_quanah
+echo -e "\nTest with 32 proc\n"
+./Q2/gaussOmp 2000 32 > ./Q2/testout/gaussOmp_2000_32_quanah
+
+echo -e "\nTest LU\n"
+
+echo -e "\nTest with 1 proc\n"
+./Q2/LUOmp 2000 1 > ./Q2/testout/LUOmp_2000_01_quanah
+echo -e "\nTest with 2 proc\n"
+./Q2/LUOmp 2000 2 > ./Q2/testout/LUOmp_2000_02_quanah
+echo -e "\nTest with 4 proc\n"
+./Q2/LUOmp 2000 4 > ./Q2/testout/LUOmp_2000_04_quanah
+echo -e "\nTest with 8 proc\n"
+./Q2/LUOmp 2000 8 > ./Q2/testout/LUOmp_2000_08_quanah
+echo -e "\nTest with 16 proc\n"
+./Q2/LUOmp 2000 16 > ./Q2/testout/LUOmp_2000_16_quanah
+echo -e "\nTest with 32 proc\n"
+./Q2/LUOmp 2000 32 > ./Q2/testout/LUOmp_2000_32_quanah
